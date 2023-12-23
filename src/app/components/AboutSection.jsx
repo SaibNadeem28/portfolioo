b0ae93206d217b81dbb702088bd83be8a21f86d6
SaibNadeem28 <93053498+SaibNadeem28@users.noreply.github.com>
2023-12-24 02:19:16 +0500

    build errors

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -30,7 +30,7 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Bachelor's in Information Technology</li>
+        <li>Bachelor&apos;s in Information Technology</li>
         <p className="text-xs italic font-thin">Bahria University, Lahore</p>
         <p className="text-xs italic font-thin">2019 - 2023</p>
         <li>Fsc Pre-Engineering</li>
@@ -80,7 +80,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about.png" width={500} height={500} />
+        <Image src="/images/about.png" alt="" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
